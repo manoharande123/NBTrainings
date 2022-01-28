8fa3886ba3065a78a4e8247749f9837931a96ad4
manohar123 <manohar.ande96@gmail.com>
2022-01-28 12:29:44 +0530

    fourth submission

---
+++ DAY3 ASSIGNMENTS/While multiplication program/While multiplication program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_multiplication_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input, i = 1;
+            Console.WriteLine("enter number");
+            input = Convert.ToInt32(Console.ReadLine());
+            while (i <= 10)
+            {
+                Console.WriteLine("{0}x{1}={2}", input, i, input * i);
+                i++;
+
+            }
+            Console.ReadLine();
+               
+        }
+    }
+}
