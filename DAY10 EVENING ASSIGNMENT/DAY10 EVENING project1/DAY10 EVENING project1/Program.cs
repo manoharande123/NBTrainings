a1442005aa6a1d6fc2646c95f3a8c470e2cee8f8
manohar123 <manohar.ande96@gmail.com>
2022-02-06 12:42:24 +0530

    DAY10 ASSIGNMENT

---
+++ DAY10 EVENING ASSIGNMENT/DAY10 EVENING project1/DAY10 EVENING project1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY10_EVENING_project1
+{
+    // AUTHOR : ANDE MANOHAR
+    //PURPOSE: To create an Abstract class
+    abstract class Salary
+    {
+
+        public int GetPF(int basic)
+        {
+            return 12 * basic / 100;
+        }
+        public int GetHRA(int basic)
+        {
+            return 40 % basic / 100;
+        }
+        public abstract  int GetCA();
+        public  abstract int GetSA();
+       
+
+    }
+
+    class Microsoft : Salary
+    {
+        public override int GetCA()
+        {
+            return 6000;
+        }
+
+        public override int GetSA()
+        {
+            return 7000;
+        }
+    }
+    class Google : Salary
+    {
+
+        public override int GetCA()
+        {
+            return 8000;
+        }
+        public override int GetSA()
+        {
+            return 10000;
+        }
+    }
+    class IBM : Salary
+    {
+
+        public override  int GetCA()
+        {
+            return 4000;
+        }
+        public override int  GetSA()
+        {
+            return 6000;
+        }
+
+    }
+    class Facebook : Salary
+    {
+
+        public override int GetCA()
+        {
+            return 12000;
+        }
+        public override int GetSA()
+        {
+            return 8000;
+        }
+
+    }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Microsoft
+            //Google
+            //IBM
+            //Facebook
+
+            Console.WriteLine("Complted processing");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DAY10 MORNING ASSIGNMENT/DAY10 PROJECT4/DAY10 PROJECT4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY10_PROJECT4
+{
+    class Indianexpress
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Good morning");
+        }
+        public void Printdata()
+        {
+            Console.WriteLine("HeadLines");
+        }
+        public  void Printmain()
+        {
+            Console.WriteLine("Descripion");
+        }
+    }
+    class Enaadu : Indianexpress
+    {
+       public override void Print()
+        {
+            Console.WriteLine("shubodayam");
+        }
+    }
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enaadu obj = new Enaadu();
+            obj.Print();
+            Console.ReadLine();
+        }
+    }
+}
