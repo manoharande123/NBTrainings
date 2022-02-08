bab31d050bba208fd2937407fde490d870474e40
manohar123 <manohar.ande96@gmail.com>
2022-02-08 19:44:01 +0530

    DAY12 ASSIGNMENT

---
+++ DAY12 ASSIGNMENT/DAY 12 PROJECT1/DAY 12 PROJECT1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_12_PROJECT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+
+            {
+                int a, b, c;
+                Console.WriteLine("Enter first number:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("division of two numbers is {0}", c);
+                Console.ReadLine();
+
+            }
+            catch (OverflowException)// This is for overflow exception
+
+            {
+                Console.WriteLine("Only nummbers between 0 to 900000");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only numbers are allowed");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine(" Can not divide with zero");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("some error occured please contact admin");
+            }
+            finally
+            {
+                Console.WriteLine(" Designed by Manohar");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DAY12 ASSIGNMENT/DAY12 PROJECT/DAY12 PROJECT/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY12_PROJECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            try
+            {
+                Console.WriteLine("enter a:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter b:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("Division of two numbers is {0}", c);
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("enter numbers between 0 o 600000");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Number cant be divided by zero");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter numbers only");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong, Manohar");
+
+            }
+            finally
+            {
+                Console.WriteLine("\n \n \n \n \n Designed by  Manohar");
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
