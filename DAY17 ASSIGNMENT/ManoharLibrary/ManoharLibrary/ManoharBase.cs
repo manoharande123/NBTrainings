70e34c25cb77738dd8c153e50b829b6243b67437
manohar123 <manohar.ande96@gmail.com>
2022-02-16 06:50:15 +0530

    DAY17 ASSIGNMENT

---
+++ DAY17 ASSIGNMENT/ManoharLibrary/ManoharLibrary/ManoharBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManoharLibrary
+{
+    //BASECLASS
+    public class ManoharBase
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        
+
+        
+        public void ReadData()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+    //DERIVEDCLASS
+    public class ManoharDerived: ManoharBase
+    {
+       
+        public void ReadDerivedDaa()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+    //OTHERCLASS
+    public class ManoharOther
+    {
+        
+       //Otherclass
+
+        public void ReadOtherData()
+        {
+            ManoharBase m = new ManoharBase();
+                m.a = 1;
+                m.b = 2;
+                m.c = 3;
+                m.d = 4;
+                m.e = 5;           
+        }
+
+
+    }
+
+
+    
+}
---
+++ DAY17 ASSIGNMENT/ManoharLibrary/PublicLibrary/Publicbase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManoharLibrary;
+
+namespace PublicLibrary
+{
+
+    //derivedclass
+    public class Publicbase:ManoharBase
+    {
+        
+               public void PublicData()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+        }
+    }
+    //otherclass
+    public class PublicOther
+    {
+        public void ReadOher()
+        {
+            ManoharBase m = new ManoharBase();
+            m.a = 1;
+            m.b = 2;
+            m.c = 3;
+            m.d = 4;
+            m.e = 5;
+        }
+    }
+}
