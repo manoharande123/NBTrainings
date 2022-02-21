d3ed8772081b340eb72d3d7b098fa78dd31a0723
manohar123 <manohar.ande96@gmail.com>
2022-02-21 22:43:45 +0530

    DAY21 ASSIGNMENT

---
+++ DAY21 ASSIGNMENT/MyClientApp/MyClientApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyClientApp.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient obj = new WebService1SoapClient();
+            Console.WriteLine(obj.Factorial(6));
+            Console.WriteLine(obj.Add(5,5));
+            Console.WriteLine(obj.Mul(2,3));
+            Console.WriteLine(obj.Div(10,2));
+
+            Console.ReadLine()
+        }
+    }
+}
---
+++ DAY21 ASSIGNMENT/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.ServiceReference1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebService1SoapClient obj = new WebService1SoapClient();
+            int n = Convert.ToInt32(textBox1.Text);
+            textBox2.Text = obj.Factorial(n).ToString();
+        }
+    }
+}
