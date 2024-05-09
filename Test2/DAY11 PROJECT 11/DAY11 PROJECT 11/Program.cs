1e9227647beedea8d53d41dd74ca9f3919ed2f25
Manohar Ande <mande@nationsbenefits.com>
2024-05-09 08:57:58 -0400

    Deleted Test Folder

---
+++ Test2/DAY11 PROJECT 11/DAY11 PROJECT 11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY11_PROJECT_11
+{
+    class Mathematics
+    {
+        public static int Add(int a , int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics math = new Mathematics();
+            Console.WriteLine($"Addiion={Mathematics.Add(12,6)}");
+            Console.WriteLine($"Difference= {Mathematics.Sub(10, 4)}");
+            Console.WriteLine($"Product= {Mathematics.Product(5, 4)}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
